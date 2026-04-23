89493f21d43a4e6feaa451a3d547e572df1e7f2f
metanetabasova <metanetabasova314@gmail.com>
2026-04-23 15:41:05 +0000

    Add 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        ascii_code = ord(char)
+        if ascii_code >= 97 and ascii_code <= 122:
+            char = chr(ascii_code - 32)
+        print("{}".format(char), end="")
+    print("")
