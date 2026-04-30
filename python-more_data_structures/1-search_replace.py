ea9d951587a9b9e8605f5ea1ceff7008ce0a9a7d
metanetabasova <metanetabasova314@gmail.com>
2026-04-30 10:54:37 +0000

    Add 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if x == search else x for x in my_list]
+    """Replaces all occurrences of an element by another in a new list"""
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+    return new_list
