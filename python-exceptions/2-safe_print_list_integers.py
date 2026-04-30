c8ea15d9c78b2604dcd5467940674949281fce89
metanetabasova <metanetabasova314@gmail.com>
2026-04-30 13:18:58 +0000

    Add 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """Siyahıdan x qədər elementə baxır və yalnız tam ədədləri çap edir."""
     printed_integers = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            value = my_list[i]
+            print("{:d}".format(value), end="")
             printed_integers += 1
-        except (ValueError, TypeError):
-            continue
         except IndexError:
             break
+        except (ValueError, TypeError):
+            continue
     print("")
     return printed_integers
