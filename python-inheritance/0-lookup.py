3c61bafdd4ea7a78280bb727089ca2e655ddd404
metanetabasova <metanetabasova314@gmail.com>
2026-05-06 19:33:37 +0000

    Add 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''bu modul obyejtlerin atributlarini yoxlamaq ucun funksiyadan ibaretdir'''
 def lookup(obj):
     '''Obyektin butun atribut ve metodlarinin siyahisini qaytarir'''
     return dir(obj)
