a4a689148e04b671f7725127f55dee7e9c83e300
metanetabasova <metanetabasova314@gmail.com>
2026-05-07 15:37:41 +0000

    Add 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''BaseGeometry sinfini tekmilesdiren modul'''
+
+
+class BaseGeometry:
+    '''hendesi fiqurlar ucun baza sinfi'''
+
+    def area(self):
+        '''saheni hesablayan metod'''
+        raise Exception("area() is not implemented")
