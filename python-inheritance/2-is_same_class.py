624633396f7e1dc4dafa39c4162faa6aaa4a9ecf
metanetabasova <metanetabasova314@gmail.com>
2026-05-07 10:38:21 +0000

    Add 2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''bu modul obyektin sinifini yoxlayan funksiyadan ibaretdir.'''
+
+
+def is_same_class(obj, a_class):
+    '''obyektin tam olaraq gosterilen sinifin numunesi olub olmadigini yoxlayir'''
+return type(obj) is a_class
