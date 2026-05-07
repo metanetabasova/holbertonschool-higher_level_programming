5f80894b7f62a31e9ac3a05ca098a3840b069b5f
metanetabasova <metanetabasova314@gmail.com>
2026-05-07 13:57:37 +0000

    Add 3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''obyektin sinif elaqesini yoxlayan'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''obyektin bir sinifden olub olmadigini  yoxlayir'''
+    return isinstance(obj, a_class)
