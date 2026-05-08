b836465f64857ac692f39622d9083c458c1308e9
metanetabasova <metanetabasova314@gmail.com>
2026-05-08 20:00:06 +0000

    Add 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-'''
-Rectangle-dan miras alan Square sinfi
-'''
-Rectangle = __import__('9-rectangle')/Rectangle
+"""
+Bu modul Square sinfini təyin edir.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    '''Square sinifi'''
+    """Square sinfi üçün sənədləşdirmə."""
 
     def __init__(self, size):
-        '''
-        Yeni Square yaradir
-        '''
+        """
+        Yeni Square nümunəsini başladır.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
-        '''Kvadratin sahesini hesablayir'''
+        """Square sinfi üçün sahəni hesablayan metod."""
         return self.__size ** 2
