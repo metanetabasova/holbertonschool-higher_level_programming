4809dcd465c1cf12bf2875b80c4a33c4c3386d88
metanetabasova <metanetabasova314@gmail.com>
2026-05-09 07:41:00 +0000

    Add

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -15,7 +15,7 @@ class Shape(ABC):
 # Konkret sinifler
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
         return math.pi * self.radius ** 2
