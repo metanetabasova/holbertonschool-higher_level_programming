cdf401811a11a0425701189ed65e0bf522cebc18
metanetabasova <metanetabasova314@gmail.com>
2026-05-10 18:35:10 +0000

    Add task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class CountedIterator:
+    def __init__(self, iterable):
+        #orjinal iteratoru yaradiriq
+        self.iterator = iter(iterable)
+        # Saygaci sifirlayiriq
+        self.count = 0
+
+    def get_count(self):
+        # Cari sayi qaytarir
+        return self.count
+
+    def __next__(self):
+        try:
+            # Orijinal iteratordan novbeti elementi aliriq
+            item = next(self.iterator)
+            # Eger element varsa, saygaci artiririq
+            self.count += 1
+            return item
+        except StopIteration:
+            # Eger element bitibse, stopIteration xetasini otururuk
+            raise StopIteration
+            
+    def __iter__(self):
+        # Obyektin ozunu iterator kimi qaytaririq
+        return self
+
+# --- Test hissesi ---
+data = ["alma", "armud", "gilas"]
+counted_iter = CountedIterator(data)
+
+print(next(counted_iter))
+print(next(counted_iter))
+print(f"Oxunmus element sayi: {CountedIterator(counted_iter.get_count())}")
+
+# Qalanlari dovrle oxuyaq
+for fruit in counted_iter:
+    print(fruit)
+
+print(f"Yekun say: {counted_iter.get_count()}")
