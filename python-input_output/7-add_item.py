bf8e0d90e0d0757a1a942b087dd8d5b2d92f0d61
metanetabasova <metanetabasova314@gmail.com>
2026-05-11 13:41:31 +0000

    Add 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,20 +5,20 @@ Bu skript butun komanda setri arqumentlerini PYTHON siyahisina elave edir.
 import sys
 import os
 
-#evvelki tapsiriqdaki funksiyalari idxal edirik
+# evvelki tapsiriqdaki funksiyalari idxal edirik
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-#Eger fayl movcuddursa movcud siyahini yukleyirik
+# Eger fayl movcuddursa movcud siyahini yukleyirik
 if os.path.exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
 
-#Komanda setri arqumentlerini siyahiya elave edirik
+# Komanda setri arqumentlerini siyahiya elave edirik
 items.extend(sys.argv[1:])
 
-#Yenilenmis siyahini fayla yaziriq
+# Yenilenmis siyahini fayla yaziriq
 save_to_json_file(items, filename)
