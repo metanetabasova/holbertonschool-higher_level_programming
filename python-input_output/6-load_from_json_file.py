c4548c202aaf1a930c5e9aeb9f197ae087356ac7
metanetabasova <metanetabasova314@gmail.com>
2026-05-11 13:40:05 +0000

    Add 7-add_item.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-'''
-Bu skript butun komanda setri arqumentlerini PYTHON siyahisina elave edir.
-'''
-import sys
-import os
+"""
+Bu modul JSON faylından obyekti yükləyən funksiyanı ehtiva edir.
+"""
+import json
 
-#evvelki tapsiriqdaki funksiyalari idxal edirik
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-
-#Eger fayl movcuddursa movcud siyahini yukleyirik
-if os.path.exists(filename):
-    items = load_from_json_file(filename)
-else:
-    items = []
-
-#Komanda setri arqumentlerini siyahiya elave edirik
-items.extend(sys.argv[1:])
-
-#Yenilenmis siyahini fayla yaziriq
-save_to_json_file(items, filename)
+def load_from_json_file(filename):
+    """
+    JSON faylından məlumatı oxuyur və Python obyekti kimi qaytarır.
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
