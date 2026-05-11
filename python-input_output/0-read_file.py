0f25273c4ed9d030af7639e5254046e8e165be44
metanetabasova <metanetabasova314@gmail.com>
2026-05-11 09:02:54 +0000

    Add 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+Bu modul bir fayli oxumaq ucundur
+'''
 def read_file(filename = ""):
     '''UTF8 formatinda olan  metni oxuyur ve stdout-a cap edir.'''
     with open(filename, mode = "r", encoding = "utf-8") as f:
