5e9fb3efa462fa2573a97a4620f532808e885610
metanetabasova <metanetabasova314@gmail.com>
2026-05-11 14:21:11 +0000

    Add 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''
+Bu modul Paskal Ucbucagini hesablayan funksiyani ehtiva edir.
+'''
+
+
 def pascal_triangle(n):
     '''
     n olculu Paskal ucbucagini temsil eden siyahilarin siyahisini qaytarir.
@@ -6,23 +11,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    # Ucbucagin ilkin veziyyeti triangle = [[1]]
+    triangle = [[1]]
 
     while len(triangle) < n:
-        # Sonuncu yaradilmis setri gotururuk
         prev_row = triangle[-1]
-
-        # Yeni setri formallasdiririq : hemise 1 ile baslayir
         new_row = [1]
 
-        # Evvelki setrin daxili elementlerini toplayiriq
-        for i in range(len(prew_row) - 1):
-            new_row.append(prev_row[i] + prev_row[i+1])
+        for i in range(len(prev_row) - 1):
+            new_row.append(prev_row[i] + prev_row[i + 1])
 
-        # Yeni setri 1 ole bitiririk
         new_row.append(1)
-
-        #Tamamlanmis setri ucbucaga elave edirikk
         triangle.append(new_row)
 
     return triangle
