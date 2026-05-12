7a430c4a6a16d31d7a1a1e852454cddcb98879ec
metanetabasova <metanetabasova314@gmail.com>
2026-05-12 17:31:51 +0200

    Add task_02_csv.py

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Bu modul CSV melumatlarini oxuyub JSON formatina ceviren funksiyani ehtiva edir.
+'''
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    '''
+    CSV faylini oxuyur ve terkibini data.json faylina yazir.
+    '''
+    try:
+        # CSV faylini oxumaq ucun aciriq
+        with open(csv_filename, mode='r', encoding='utf-8') as csv_file:
+            # DictReader her setri avtomatik lugete cevirir
+            csv_reader = csv.DisctReader(csv_file)
+
+            # Butun lugetleri bir siyahiya toplayiriq
+            data_list = [row for row in csv_reader]
+
+        # Siyahini JSON formatinda data.json faylina yaziriq
+        with open('data.json', mode='w', encoding='utf-8') as json_file:
+            json.dump(data_list, json_file, indent=4)
+
+        return True
+
+    except (FileNotError, IOError, PermissionError):
+        # Fay; tapilmadiqda ve ya diger giris-cixis xetalarinda False qaytarir
+        return False
