789d5770997fc11621149228fea74df905494320
metanetabasova <metanetabasova314@gmail.com>
2026-05-16 16:37:35 +0000

    Add 0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,2 @@
-#!/usr/bin/bash
-
-# MySQL istifadeci adini qeyd
-USER="istifadeci_adi"
-
-# Şifrəni daxil etmək üçün (-p) skript səndən şifrə istəyəcək
-mysql -u $USER -p -e "SHOW DATABASES;"
+-- MySQL serverindəki bütün verilənlər bazalarını siyahılayır
+SHOW DATABASES;
