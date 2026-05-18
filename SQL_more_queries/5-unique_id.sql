cbd41a50b74aacdae6ddc8892dd034b2e24c4239
metanetabasova <metanetabasova314@gmail.com>
2026-05-18 12:46:45 +0000

    Add 5-unique_id.sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- cedvel yaradir, id sutununa default 1 ve benzerdizlik serti qoyur
-CREATE TSBLE IF NOT EXISTS unique_id(
+-- 'unique_id' cədvəlini yaradır, id sütununa default 1 və bənzərsizlik (UNIQUE) şərti qoyur
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
