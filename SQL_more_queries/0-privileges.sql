0dfa3ba17a6a527b1f443797dde0625a16f822d5
metanetabasova <metanetabasova314@gmail.com>
2026-05-18 08:48:20 +0000

    add 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- icazeleri siralayir
+-- 'user_0d_1' və 'user_0d_2' istifadəçilərinin bütün icazələrini siyahılayır
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localho';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
