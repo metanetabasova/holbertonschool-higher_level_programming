2009d37b97efbc780bf2e2a9da00bff0e4964e69
metanetabasova <metanetabasova314@gmail.com>
2026-05-18 13:49:45 +0000

    Add 12-no_genre.sql

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- janri olmayan butun TV soularini siyahiliyir
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFt JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
