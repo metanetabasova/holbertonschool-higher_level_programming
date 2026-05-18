e1f9463a4cc2e26d81b69c6b99223034256eea11
metanetabasova <metanetabasova314@gmail.com>
2026-05-18 13:18:57 +0000

    Add 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
--- Serhleri siralayir(adi California)
+-- 'cities cedvelinden adi California olan stata aid seherleri siyahiliyir
 SELECT id, name
 FROM cities
-WHERE states_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
