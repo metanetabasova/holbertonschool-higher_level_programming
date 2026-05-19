69399320be58eaf5d1d2acb5df4f2dfc2951e541
metanetabasova <metanetabasova314@gmail.com>
2026-05-19 20:53:43 +0000

    Add 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+id-ye gore artan siralayib siyahilayan skript
+'''
+import sys
+import MyDQLdb
+
+if __name__ == "__main__":
+    # konsoldan oturulen arqumentleri deyisenkere menimsedirik
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Verilenler bazasina qosuluruq
+    ddb = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name
+    )
+
+    # sorgularin icra etmek ucun cursor yaradiriq
+    cursor = db.cursor()
+
+    # SQL sorgusunu icra edirik
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Butun neticeleri elde edirik
+    query_rows = cursor.fetchall()
+
+    # Neticeleri teleb olunan formatda ekrana cixaririq
+    for row in query_rows:
+        print(row)
+
+    # Baglantilari ve cursor-u baglayiriq
+    cursor.close()
+    db.close()
