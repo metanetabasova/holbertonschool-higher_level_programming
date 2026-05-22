acacc857a551a1188386cd4db2cf0aa826426d89
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 09:47:24 +0000

    Add 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,37 +7,27 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Arqumentləri əmr sətrindən alırıq
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Verilənlər bazasına qoşuluruq
+    # Verilənlər bazasına qoşulma
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
+    
     cursor = db.cursor()
-
-    # Checker-in tam olaraq qəbul etdiyi və heç bir xəta verməyən SQL sorğusu
-    # BINARY istifadə etmək bəzən bəzi testlərdə problem yaradır, standard LIKE kifayətdir
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-    cursor.execute(query)
-
-    # Nəticələri alırıq
+    
+    # Bütün ştatları id sırası ilə seçirik
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    
     rows = cursor.fetchall()
     
-    # Ştatları tələb olunan formatda çap edirik
+    # Python ilə ciddi şəkildə böyük 'N' hərfi ilə başlayanları yoxlayırıq
     for row in rows:
-        # Əgər bazada 'Nevada' və ya 'New York' kimi ştatlar 'N' ilə başlayırsa
-        # LIKE 'N%' bəzi konfiqurasiyalarda kiçik 'n'-i də götürə bilər, zəmanət üçün yoxlayırıq:
         if row[1][0] == 'N':
             print(row)
-
-    # Əlaqələri bağlayırıq
+            
+    # Bağlanışlar
     cursor.close()
     db.close()
