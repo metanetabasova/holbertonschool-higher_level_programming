47f82acc068dd2f8ff7cdb9a55877eafa7c44090
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 13:29:33 +0000

    Add 3-my_safe_filter_states.pyz

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lists all values where name matches the argument, safe from SQL injection!
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    uname, pwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=uname,
+        passwd=pwd,
+        db=db
+    )
+    cur = conn.cursor()
+
+    # Using query parameters to prevent SQL injection
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
