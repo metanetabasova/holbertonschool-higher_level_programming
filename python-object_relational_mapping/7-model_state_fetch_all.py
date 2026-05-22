de6f3633c7caf458cf35c9b91ac999a598eef1fd
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 18:10:02 +0000

    Add 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,26 +13,24 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-def main():
-    """Main entry point."""
-    if len(sys.argv) != 4:
-        return
-
+if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
 
-    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, password, dbname)
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        user, password, dbname
+    )
     engine = create_engine(url, pool_pre_ping=True)
+
+    # Make sure Base is used (avoids pycodestyle F401) and tables exist
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
         print("{}: {}".format(state.id, state.name))
 
     session.close()
-
-
-if __name__ == "__main__":
-    main()
