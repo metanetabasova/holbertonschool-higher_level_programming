884ac16232a8798ad6e59360ba89164fe3205a9c
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 09:52:37 +0000

    Fix script query

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,6 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Verilənlər bazasına qoşulma
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -15,21 +14,10 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    
     cursor = db.cursor()
-    
-    # Checker-in tam olaraq gözlədiyi "SQL injection-a qarşı təhlükəsiz" və 
-    # Böyük 'N' hərfini dəqiq ayıran BINARY strukturu:
-    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
-    
-    # %s yerinə 'N%' parametrini təhlükəsiz şəkildə ötürürük (tuple olaraq)
-    cursor.execute(query, ('N%',))
-    
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC", ('N%',))
     rows = cursor.fetchall()
-    
     for row in rows:
         print(row)
-        
-    # Kursor və bazanı bağlayırıq
     cursor.close()
     db.close()
