cfeb28416aa144da1003092bb88184df23263800
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 12:25:09 +0000

    Add 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""
-Lists all values in the states table where name matches the argument.
-"""
+"""Lists all values in the states table where name matches the argument."""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     uname, pwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=uname,
-        passwd=pwd,
-        db=db
-    )
+    conn = MySQLdb.connect(host="localhost", port=3306, user=uname, passwd=pwd, db=db)
     cur = conn.cursor()
-
-    # tək dırnaq problemini (SQL injection və s.) önləmək üçün
-    state_name = state_name.replace("'", "''")
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    # .format() ilə, tək dırnaq ikiləşdirilərək escape edilir
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name.replace("'", "''"))
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
