707402c6245ffd0a2dd0c2043cfff6bdce768f1d
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 16:45:27 +0000

    Add 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,46 @@
 #!/usr/bin/python3
 """
-Lists all cities of a state given as argument (safe from SQL injection).
-"""
-import MySQLdb
-import sys
+Lists all cities of a given state from the database hbtn_0e_4_usa.
 
-if __name__ == "__main__":
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+Usage: ./5-filter_cities.py <mysql_username> <mysql_password> <database_name> <state_name>
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    if len(sys.argv) != 5:
+        return
+
+    username, password, dbname, state_name = sys.argv[1:5]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=db_name
+        db=dbname
     )
     cur = db.cursor()
 
-    # Yalnız bir sorğu, JOIN ilə
-    query = """
+    # One execute() call, parameterized (SQL injection safe)
+    cur.execute(
+        """
         SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC
-    """
-    cur.execute(query, (state_name,))
+        """,
+        (state_name,)
+    )
 
     rows = cur.fetchall()
-    # Çıxış: şəhər adlarını vergüllə ayırıb bir sətrdə çap et
     print(", ".join([row[0] for row in rows]))
 
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
