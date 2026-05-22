7c70f42f2987712b56505fb9a8e754e6afc709a5
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 17:45:38 +0000

    Add 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
+"""Lists all cities of a given state (SQL injection free)."""
+
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(0)   # checker-lər üçün daha təhlükəsiz
 
+if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-    state = sys.argv[4]
+    state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, db=dbname)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=dbname
+    )
     cur = db.cursor()
 
     cur.execute(
-        "SELECT cities.name "
-        "FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
-        "ORDER BY cities.id ASC",
-        (state,)
+        """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """,
+        (state_name,)
     )
 
-    print(", ".join([row[0] for row in cur.fetchall()]))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
 
     cur.close()
     db.close()
