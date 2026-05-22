dd293154b5aaaa7b07e04bf4e3ad4fd17256b108
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 16:42:42 +0000

    Add 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
 """
-Script that lists all cities of a specific state from database hbtn_0e_4_usa
+Lists all cities of a state given as argument (safe from SQL injection).
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=username,
         passwd=password,
-        db=database
+        db=db_name
     )
+    cur = db.cursor()
 
-    cursor = db.cursor()
+    # Yalnız bir sorğu, JOIN ilə
+    query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+    cur.execute(query, (state_name,))
 
-    cursor.execute("""SELECT cities.id, cities.name
-                      FROM cities
-                      JOIN states ON cities.state_id = states.id
-                      WHERE states.name = %s
-                      ORDER BY cities.id ASC""", (state_name,))
+    rows = cur.fetchall()
+    # Çıxış: şəhər adlarını vergüllə ayırıb bir sətrdə çap et
+    print(", ".join([row[0] for row in rows]))
 
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
