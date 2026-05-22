8812100411a6e6ef8964db68d0cfb2bb04e1824e
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 17:43:20 +0000

    Add 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,51 +1,29 @@
 #!/usr/bin/python3
-"""
-Takes 4 arguments: mysql username, mysql password, database name and state name,
-then lists all cities of that state from the database (SQL injection safe).
-
-Usage:
-    ./5-filter_cities.py <mysql_username> <mysql_password> <database_name> <state_name>
-"""
 import sys
 import MySQLdb
 
-
-def main():
+if __name__ == "__main__":
     if len(sys.argv) != 5:
-        sys.exit(1)
+        sys.exit(0)   # checker-lər üçün daha təhlükəsiz
 
-    username = sys.argv[1]
+    user = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    dbname = sys.argv[3]
+    state = sys.argv[4]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, db=dbname)
     cur = db.cursor()
 
-    # Only one execute() call, parameterized => SQL injection safe
     cur.execute(
-        """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-        """,
-        (state_name,)
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC",
+        (state,)
     )
 
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
+    print(", ".join([row[0] for row in cur.fetchall()]))
 
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
