28fc5277efb5ab11365e9bd5d115703e02c3c825
metanetabasova <metanetabasova314@gmail.com>
2026-05-22 17:19:27 +0000

    Add 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """
 Lists all cities of a given state from the database hbtn_0e_4_usa.
-
-Usage: ./5-filter_cities.py <mysql_username> <mysql_password> <database_name> <state_name>
+Usage: ./4-cities_by_state.py <mysql_username> <mysql_password>
+                               <database_name> <state_name>
 """
-import sys
 import MySQLdb
+import sys
 
 
-def main():
-    if len(sys.argv) != 5:
-        return
-
-    username, password, dbname, state_name = sys.argv[1:5]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=dbname
+        db=database
     )
-    cur = db.cursor()
 
-    # One execute() call, parameterized (SQL injection safe)
-    cur.execute(
+    cursor = db.cursor()
+
+    cursor.execute(
         """
-        SELECT cities.name
+        SELECT cities.id, cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
@@ -35,12 +35,9 @@ def main():
         (state_name,)
     )
 
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-    cur.close()
+    cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
