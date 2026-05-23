cb7835251d4e8e961e55d1c6dddd2efd7bed2a58
metanetabasova <metanetabasova314@gmail.com>
2026-05-23 18:10:48 +0000

    Son

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that lists all State objects from the database hbtn_0e_6_usa
-"""
+"""Script that lists all State objects from the database hbtn_0e_6_usa."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,31 +8,30 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql_user> <mysql_password> <database>")
+        print("Usage: ./0-select_states.py <user> <password> <database>")
         sys.exit(1)
 
     mysql_user = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Create engine
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            mysql_user, mysql_password, database_name),
-        pool_pre_ping=True)
+            mysql_user,
+            mysql_password,
+            database_name
+        ),
+        pool_pre_ping=True
+    )
 
-    # Create tables if they don't exist
     Base.metadata.create_all(engine)
 
-    # Create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and display states sorted by id
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
-    # Close session
     session.close()
