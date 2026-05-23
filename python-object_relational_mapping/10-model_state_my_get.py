8d03c3b5ece75c07e364a8326ecf7cfe2b4bd3ef
metanetabasova <metanetabasova314@gmail.com>
2026-05-23 19:07:07 +0000

    Add 10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Print the State id for the State name passed as argument.
+
+Usage:
+    ./10-model_state_my_get.py <mysql_username> <mysql_password> <database_name> <state_name>
+"""
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, password, dbname
+    )
+    engine = create_engine(url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+
+    session.close()
