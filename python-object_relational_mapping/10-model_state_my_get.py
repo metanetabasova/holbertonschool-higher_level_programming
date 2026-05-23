1cdf48815a22ac3c1c10919cb796f4756a9e871b
metanetabasova <metanetabasova314@gmail.com>
2026-05-23 19:11:28 +0000

    Add

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python3
-"""Script that prints State object with name passed as argument."""
+"""Print the State id for the State name passed as argument."""
 import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from model_state import Base, State
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./3-my_safe_filter_states.py <user> <password> <db> <state>")
-        sys.exit(1)
-
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
     state_name = sys.argv[4]
 
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            mysql_user,
-            mysql_password,
-            database_name
-        ),
-        pool_pre_ping=True
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, password, dbname
     )
-
-    Base.metadata.create_all(engine)
+    engine = create_engine(url, pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(
-        State.name == state_name
-    ).first()
+    state = session.query(State).filter(State.name == state_name).first()
 
     if state is None:
         print("Not found")
     else:
-        print("{}: {}".format(state.id, state.name))
+        print(state.id)
 
     session.close()
