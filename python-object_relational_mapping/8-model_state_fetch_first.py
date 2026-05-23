3e8fc076dede10110465aeb43a3527582c9c367f
metanetabasova <metanetabasova314@gmail.com>
2026-05-23 18:19:29 +0000

    Add 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""
-Print the first State object from the database.
-
-Usage:
-    ./8-model_state_fetch_first.py <mysql_username> <mysql_password> <database_name>
-"""
+"""Script that prints the first State object from the database."""
 import sys
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from model_state import Base, State
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
+    if len(sys.argv) != 4:
+        print("Usage: ./1-first_state.py <user> <password> <database>")
+        sys.exit(1)
 
-    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        username, password, dbname
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            mysql_user,
+            mysql_password,
+            database_name
+        ),
+        pool_pre_ping=True
     )
-    engine = create_engine(url, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    first_state = session.query(State).order_by(State.id).first()
+    state = session.query(State).order_by(State.id).first()
 
-    if first_state is None:
+    if state is None:
         print("Nothing")
     else:
-        print("{}: {}".format(first_state.id, first_state.name))
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
