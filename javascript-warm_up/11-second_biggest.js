6cdebd4b223db6a6f13cf9050c233eadb00d8f1a
metanetabasova <metanetabasova314@gmail.com>
2026-06-04 15:07:17 +0000

    Add 11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const args = process.argv.slice(2).map(x => parseInt(x, 10));
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const uniqueArgs = [...new Set(args)];
+  uniqueArgs.sort((a,b) => b - a);
+
+  if (uniqueArgs.length < 2) {
+    console.log(0);
+  } else {
+    console.log(uniqueArgs[1]);
+  }
+}
