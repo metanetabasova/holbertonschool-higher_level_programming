e4fad54b6c445787a08213d3ee37a905861ff8c3
metanetabasova <metanetabasova314@gmail.com>
2026-06-25 10:40:07 +0000

    Add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module provides a function to add two float numbers."""
+
+
 def add(a: float, b: float) -> float:
     """Returns the sum of two float numbers."""
     return a + b
