da9fb86c4c9d2f07e351833ad8f6775eefd6d76a
metanetabasova <metanetabasova314@gmail.com>
2026-06-25 10:42:48 +0000

    Add 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env pythhon3 
+"""This module provides a function to concatenate two string."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two string and return the result."""
+    return str1 + str2
