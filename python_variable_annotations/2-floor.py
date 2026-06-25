c95e96d577d902bb14f9d59dad4a11b6eaea322a
metanetabasova <metanetabasova314@gmail.com>
2026-06-25 10:49:01 +0000

    Add

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This modeule provides a function to find the floor of a float."""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor the given float number."""
+    return math.floor(n)
