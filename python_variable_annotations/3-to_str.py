502fe0e3321f34adf8a8683e0ff9f04b5c78f491
metanetabasova <metanetabasova314@gmail.com>
2026-06-25 10:51:40 +0000

    Add

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module provides a function to convert a float to a string."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of the given float."""
+    return str(n)
