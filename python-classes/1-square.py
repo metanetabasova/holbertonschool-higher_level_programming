4ec2b2fda7e4f2c53d0a7ab12d21d989350b7fe2
metanetabasova <metanetabasova314@gamil.com>
2026-07-08 13:28:52 +0000

    Add

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides a Square class.
+"""
+
+
 class Square:
     """A class that defines a square."""
-    
+
     def __init__(self, size):
         """Initialize the square with a private size attribute.
-        
+
         Args:
             size: The size of a side of the square.
         """
